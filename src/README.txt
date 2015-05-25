f0036b70ea81adb779cf8a3dac67e740d727a87b
bolinkd <williambolink@gmail.com>
2015-05-25 13:23:38 -0700

    Final Submission Changes

---
+++ src/README.txt
@@ -0,0 +1,18 @@
+Name: Dustin Bolink
+Student Id: V00747883
+
+Instructions to run main program:
+1) run make
+2) run ./main
+
+Capabilities of the shell:
+1) Runs and executable using the users path or given the location of the executable
+2) Change directories using cd <directory>
+3) Run processes in background using bg <command>
+4) List processes currently running in backgroung using bglist
+
+***BONUS***
+5) Kill processes using kill <pid>
+	a) only allows killing of processes created by this shell
+6) Pause processes using pause <pid>
+7) Resume processes using resume <pid>
--- src/main.c
+++ src/main.c
@@ -10,7 +10,10 @@
 
 
 
-
+/*************
+* Struct to hold Processes
+* Includes: pid,pname,jobnum, status
+*************/
 typedef struct{
 	struct process *next;
 	struct process *prev;
@@ -20,12 +23,22 @@ typedef struct{
 	char status[10];
 }process;
 
-
+/*************
+* Initialize head and tail of
+* Processes Linked List
+*************/
 process *head = NULL;
 process *curr = NULL;
 
+// Variable to detect when a process finishes
 sig_atomic_t child_exit_status;
 
+/*************
+* Signature: 
+* 	process * findProcess(pid_t pid);
+* Accepts: pid
+* Action: Returns process with pid
+*************/
 process * findProcess(pid_t pid){
 	process* finder = head;
 	while (finder != NULL){
@@ -37,6 +50,12 @@ process * findProcess(pid_t pid){
 	return NULL;
 }
 
+/*************
+* Signature: 
+* 	void printProcesses();
+* Accepts: nothing
+* Action: prints all processes in processes list
+*************/
 void printProcesses(){
 	process *toprint = head;
 	if(toprint == NULL){
@@ -49,20 +68,32 @@ void printProcesses(){
 	}
 }
 
+/*************
+* Signature: 
+* 	void emptylist();
+* Accepts: nothing
+* Action: Emptys and frees all pointers in processes list
+*************/
 void emptylist(){
-	curr = head;
+//	curr = head;
 	process *tofree = head;
-	while(tofree != NULL){
-		process *temp = tofree;
+	while(head != NULL){
+		tofree = head;
+		pid_t pid = tofree->processid;
+		kill(pid,SIGKILL);
+		head = (process *)head->next;
+		free(tofree);
 		fprintf(stderr,"Process with id %i forcibly closed\n",tofree->processid);
-		tofree = (process *)tofree->next;
-		killProcess(temp->processid);
-		free(temp);	
 	}
-	free(head);
 }
 
 
+/*************
+* Signature: 
+* 	void addProcess(pid_t pid, char* name, int *jobnum);
+* Accepts: pid, name, jobnum
+* Action: adds process with above info at end of processes list
+*************/
 void addProcess(pid_t pid, char* name, int *jobnum){
 
 	*jobnum += 1;
@@ -92,6 +123,12 @@ void addProcess(pid_t pid, char* name, int *jobnum){
 	 
 }
 
+/*************
+* Signature: 
+* 	void pauseProcess(pid_t pid);
+* Accepts: pid
+* Action: pauses process with above id
+*************/
 void pauseProcess(pid_t pid){
 	process* topause = findProcess(pid);
 	if (topause == NULL){
@@ -107,7 +144,12 @@ void pauseProcess(pid_t pid){
 	}
 }
 
-
+/*************
+* Signature: 
+* 	void resumeProcess(pid_t pid);
+* Accepts: pid
+* Action: resumes process with above id
+*************/
 void resumeProcess(pid_t pid){
 	process* toresume = findProcess(pid);
 	if (toresume == NULL){
@@ -123,7 +165,12 @@ void resumeProcess(pid_t pid){
 	}
 }
 
-
+/*************
+* Signature: 
+* 	void removeProcess(pid_t pid);
+* Accepts: pid
+* Action: removes process with above id from processes list
+*************/
 int removeProcess(pid_t pid){
 	process* toremove = findProcess(pid);
 	if (toremove == NULL) return 0;
@@ -145,6 +192,12 @@ int removeProcess(pid_t pid){
 	return 1;
 }
 
+/*************
+* Signature: 
+* 	void clean_up_child_process (int signal_number);
+* Accepts: signal
+* Action: Handler, removes process from list when process completes
+*************/
 void clean_up_child_process (int signal_number){
 	pid_t pid;
 	while((pid = waitpid((pid_t)(-1), NULL, WNOHANG)) > 0){
@@ -153,31 +206,50 @@ void clean_up_child_process (int signal_number){
 	}
 }
 
+/*************
+* Signature: 
+* 	void killProcess(pid_t pid);
+* Accepts: pid
+* Action: sends temination signal to process
+*************/
 int killProcess(int processid){
 	pid_t pid = (pid_t) processid;
 	int success = removeProcess(pid);
 	if(success == 0) fprintf(stderr,"No Process with id %i\n",pid);
 	if(success == 1){
-		kill(pid, 9);
+		kill(pid, SIGKILL);
 	}
 	
 }
 
 
-int main(void)
-{
 
+/*************
+* Signature: 
+* 	int main(void);
+* Accepts: nothing
+* Action: main control flow
+*		a) accept user input
+*		b) perform action based on input
+*************/
+int main(void){
+
+	//Init Variables
 	char *user_input = NULL;
 	int bailout = 0;
 	char *homeDir = getenv("HOME");
 	int jobnum = 0;
 
+	//Define Signal Handler
 	struct sigaction sigchld_action;
 	memset(&sigchld_action, 0, sizeof(sigchld_action));
 	sigchld_action.sa_handler = &clean_up_child_process;
 	sigaction(SIGCHLD, &sigchld_action, NULL);
 
+	//While user has not quit
 	while(bailout == 0){
+
+		//Init variables
 		char ** results = NULL;
 		char * input_args;
 		char prompt[PATH_MAX + 10];
@@ -186,23 +258,31 @@ int main(void)
 		pid_t pid;
 		char *currentDir =getcwd(NULL,0);
 
+		//Create Prompt for user
 		strcpy(prompt,"RSI: ");
 		strcat(prompt,currentDir);
 		strcat(prompt, " >");
 
+		//Prompt user for input
 		user_input = readline(prompt);
 		
+		//Parse Input
 		input_args = strtok(user_input, " ");
+
+		//If starts with bg raise flag
 		if (strcmp(input_args, "bg") == 0){
 			bg_tag = 1;
 			input_args = strtok(NULL," ");
-		}
+		}//end if
+
+		//if not commands raise error
 		if(input_args == NULL){
 			fprintf(stderr,"No command given\n");
 			continue;
-		}
+		}//end if
+		
+		//Parse Inputs
 		while(input_args){
-
 			results = realloc(results, sizeof(char*) * ++num_args);
 			if (results == NULL) exit(-1);
 			if (strncmp(input_args,"~",1)==0){
@@ -213,25 +293,29 @@ int main(void)
 				results[num_args-1] = out;
 			}else{
 				results[num_args-1] = input_args;
-			}
+			}//end if
 			input_args = strtok(NULL," ");
-		}
-
+		}//end While
+		
+		//Add null string pointer to results (for use with execvp())
 		results = realloc(results, sizeof(char*) * ++num_args);
-		if (results == NULL) exit(-1);
+
+		//If no information entered
+		if (results == NULL) fprintf(stderr, "No Commands Given");
 		results[num_args-1] = 0;
 		
+		//Check first input for commands		
 		if(strcmp(results[0], "cd") == 0){
 			if(results[1] == 0){
 				fprintf(stderr,"No Directory Given\n");
 				continue;
-			}
+			}//end if
 			int success;
 			success = chdir(results[1]);
 			if(success == -1){
 				fprintf(stderr,"Failure\n");
 				chdir(currentDir);
-			}
+			}//end if
 		}else if(strcmp(results[0], "quit") == 0){
 			bailout = 1;
 		}else if(strcmp(results[0], "bglist") == 0){
@@ -239,41 +323,35 @@ int main(void)
 		}else if(strcmp(results[0], "kill") == 0){
 			if(results[1] == NULL){
 				fprintf(stderr, "No Process id Given");
-			}
+			}//end if
 			char* garbage = NULL;
-
     			int processid= strtol(results[1], &garbage, 0);
 			if(processid == 0){
 				fprintf(stderr, "%s is not a processid\n",results[1]);
 				continue;
-			}
-
+			}//end if
 			killProcess(processid);
 		}else if(strcmp(results[0], "resume") == 0){
 			if(results[1] == NULL){
 				fprintf(stderr, "No Process id Given");
-			}
+			}//end if
 			char* garbage = NULL;
-
     			int processid= strtol(results[1], &garbage, 0);
 			if(processid == 0){
 				fprintf(stderr, "%s is not a processid\n",results[1]);
 				continue;
-			}
-
+			}//end if
 			resumeProcess(processid);
 		}else if(strcmp(results[0], "pause") == 0){
 			if(results[1] == NULL){
 				fprintf(stderr, "No Process id Given");
-			}
+			}//end if
 			char* garbage = NULL;
-
     			int processid= strtol(results[1], &garbage, 0);
 			if(processid == 0){
 				fprintf(stderr, "%s is not a processid\n",results[1]);
 				continue;
-			}
-
+			}//end if
 			pauseProcess(processid);
 		}else{
 			pid = fork();
@@ -281,29 +359,32 @@ int main(void)
 			if(pid == -1){
 				fprintf(stderr,"Problem creating process\n");
 			}else if( pid == 0 ){
+				// 
 				int success;
 				success = execvp(results[0],results);
 				if(success == -1){
 					fprintf(stderr,"Process did not complete sucessfully\n");
 					exit(1);
-				}
-			}else{
-				if(bg_tag == 1){
-					addProcess(pid,results[0],&jobnum);
-				}	
-			}
-
+				}//end if
+			}//end if
+			
+			//check tag if in background add process to list
+			//else wait for process to complete
 			if (bg_tag == 0){
 				waitpid(pid,&status,0);
-			}
+			}else{
+				addProcess(pid,results[0],&jobnum);
+			}//end if
 
 
 		}//end if
 		bg_tag = 1;
+		//Reset memory for next loop
 		free(input_args);
 		free(user_input);
 	}//end While
 
+	//Terminate memory
 	emptylist();
 	fprintf(stderr,"Goodbye!\n");
 	exit(EXIT_SUCCESS);
